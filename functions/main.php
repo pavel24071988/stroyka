c97620f5c03561e713586e7a03bde5efb03c2c82
Pavel <Pavel24071988@mail.ru>
2016-03-27 18:55:33 +0400

    Тестирование гита

--- functions/main.php
+++ functions/main.php
@@ -17,7 +17,7 @@ class Application
             $curURL = [];
             $path_to_site = 'site/controllers/main';
         }else{
-            $path_to_site = 'site/controllers/';
+            $path_to_site = 'site/controllers';
             $curURL = [];
             foreach($newURL as $key => $path){
                 $check = preg_match('/^\d+$/', $path);
@@ -26,11 +26,27 @@ class Application
                 else break;
             }
         }
-        $path_to_site = $path_to_site .'/'. implode('/',$curURL) .'/page.php';
+        if(!empty(implode('/',$curURL))) $path_to_site = $path_to_site .'/'. implode('/',$curURL) .'page.php';
+        else $path_to_site = $path_to_site .'/page.php';
         if(!file_exists($path_to_site)){
             echo 'Не удалось подключить файл.';
             exit;
         }
+        
+        // логируем заходы пользователей
+        try{
+            $DB = Application::$DB;
+            $userID = 0;
+            if(!empty($_SESSION['user'])) $userID = $_SESSION['user']['id'];
+            $log_entrance = $DB->prepare('
+              INSERT INTO logs (userid, url)
+                VALUES('. $userID .', \''. $path_to_site .'\')');
+            var_dump($log_entrance);
+            var_dump($log_entrance->execute());
+            print_r($log_entrance->errorInfo());
+            $log_entrance->execute();
+        }catch(Exception $ex){}
+        
         require_once $path_to_site;
     }
 }
