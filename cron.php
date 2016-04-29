03748423d836bb48f483f184d3f3883c1c1b4e32
Pavel <Pavel24071988@mail.ru>
2016-04-29 22:43:08 +0400

    8

--- cron.php
+++ cron.php
@@ -4,7 +4,7 @@ header('Content-Type: text/html; charset=utf-8');
 include($_SERVER['DOCUMENT_ROOT'] .'/functions/main.php');
 $application = new Application;
 $DB = Application::$DB;
-/*
+
 $kinds_of_jobs = $DB->query('SELECT * FROM kinds_of_jobs')->fetchAll();
 $users = [];
 foreach($kinds_of_jobs as $kind_of_job){
@@ -31,35 +31,36 @@ foreach($users_sort as $key => $user){
 	var_dump($key .' '. rand($min_sort_value, $diff));
 	$DB->prepare('UPDATE users SET "sort"='. rand($min_sort_value, $diff) .' WHERE "id"='. $key)->execute();
 }
-*/
+/*
 $names = ['Максим','Павел','Иван','Платон','Елисей','Денис','Сергей','Жопа','Цезарь','Марс','Марк','Энгельс','Роман','Владислав','Петюня','Жора','Жека','Кока'];
 $surnames = ['Щербаков','Демидович','Мельников','Серов','Белов','Шульц','Осипов','Павлов','Сергеев','Иванов','Деревьев','Многов','Головач'];
 $second_names = ['Андреевич','Сидорович','Сергеевич','Петрович','Владович','Чуйчич','Гуслич','Жуслич','Плохоч','Смешливович','Романович'];
 
-for($i=0; $i<3000; $i++){
+for($i=0; $i<300; $i++){
     $names_rand = $names[rand(1, 17)];
-    $surnames_rand = $surnames[rand(1, 12)];
-    $second_names_rand = $second_names[rand(1, 10)];
-
-    var_dump($names_rand .' '. $surnames_rand .' '. $second_names_rand);
-
-    $sql = $DB->prepare('
-    INSERT INTO users ("age", "areaID", "cityID", email, name, experience, status, password, second_name, surname, sort)
-      VALUES('. rand(18, 105) .', '. rand(1, 4) .', '. rand(1, 13) .', \'test_'. rand(1, 6000) .'@mail.ru\', \''. $names_rand .'\', '. rand(1, 105) .', 0, \''. md5(rand(1, 100000)) .'\', \''. $second_names_rand .'\', \''. $surnames_rand .'\', '. $i .')
+	$surnames_rand = $surnames[rand(1, 12)];
+	$second_names_rand = $second_names[rand(1, 10)];
+	
+	var_dump($names_rand .' '. $surnames_rand .' '. $second_names_rand);
+	
+	$sql = $DB->prepare('
+        INSERT INTO users ("age", "areaID", "cityID", email, name, experience, status, password, second_name, surname, sort)
+          VALUES('. rand(18, 105) .', '. rand(1, 4) .', '. rand(1, 13) .', \'test_'. rand(1, 6000) .'@mail.ru\', \''. $names_rand .'\', '. rand(1, 105) .', 0, \''. md5(rand(1, 100000)) .'\', \''. $second_names_rand .'\', \''. $surnames_rand .'\', '. $i .')
     ');
-    var_dump(1);
+	var_dump(1);
     $sql->execute();
-    $lastInsertId = $DB->lastInsertId('users_id_seq');
-    var_dump(2);
-
-    $kinds = rand(1, 5);
-
-    for($ii=0; $ii<=$kinds; $ii++){
-            $sql = $DB->prepare('
-                    INSERT INTO users_kinds_of_jobs (kind_of_job_id, "userID")
-                      VALUES('. rand(7, 35) .', '. (int) $lastInsertId .')
-            ');
-            var_dump(3);
-            $sql->execute();
-    }
+	$lastInsertId = $DB->lastInsertId('users_id_seq');
+	var_dump(2);
+	
+	$kinds = rand(1, 5);
+	
+	for($ii=0; $ii<=$kinds; $ii++){
+		$sql = $DB->prepare('
+			INSERT INTO users_kinds_of_jobs (kind_of_job_id, "userID")
+			  VALUES('. rand(1, 6) .', '. (int) $lastInsertId .')
+		');
+		var_dump(3);
+		$sql->execute();
+	}
 }
+*/
