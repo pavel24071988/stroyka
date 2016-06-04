419a64363b8cf0b81bfd3a497f3608d2349d7dad
Pavel <Pavel24071988@mail.ru>
2016-06-04 19:19:49 +0400

    1

--- site/controllers/masters/page.php
+++ site/controllers/masters/page.php
@@ -57,7 +57,7 @@ $sql = '
 $busy = 'r."status"=\'0\'';
 if(!empty($_GET['busy'])) $busy ='';
 if(!empty($_GET['cityID'])) $dopSQL[] = 'r."cityID"=\''. $_GET['cityID'] .'\'';
-if(!empty($_GET['areaID'])) $dopSQL[] = 'r."areaID"=\''. $_GET['areaID'] .'\'';
+/*if(!empty($_GET['areaID'])) $dopSQL[] = 'r."areaID"=\''. $_GET['areaID'] .'\'';*/
 if(!empty($_GET['comments'])) $dopSQL[] = 'r."comment_count">0';
 if(!empty($_GET['plus_comments'])) $dopSQL[] = 'r."plus_comment_count">0';
 if(!empty($_GET['search_str'])) $dopSQL[] = 'r."name" LIKE \'%'. $_GET['search_str'] .'%\'';
